3a254fac0b924d8f49786d862b0c9abd968e13c8
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2019-10-07 20:59:07 -0400

    update

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,7 @@ import {Animated} from "react-animated-css"
 class Landing extends React.Component {
     render() {
     	//var bg = require('./images/IMG_5210.jpg')
-    	var bg = require('./images/bot7.png')
+    	var bg = require('./images/bot9.png')
     	//var bg2 = require('./images/bg_2.jpg')
         const { onAboutClick, showLanding, onContactUsClick } = this.props
 
